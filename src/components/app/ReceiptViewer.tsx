79160886c9ca6b1f64815e96f43bb55e49805c14
Oui Wutcharin <wutcharin.th@gmail.com>
2025-08-09 07:07:46 +0000

    On the Original Receipt view, I see 2 "X" overlay. I think it's bug can

--- src/components/app/ReceiptViewer.tsx
+++ src/components/app/ReceiptViewer.tsx
@@ -27,10 +27,6 @@ const ReceiptViewer: React.FC<ReceiptViewerProps> = ({ isVisible, onClose, recei
         <DialogContent className="max-w-md w-full p-2 sm:p-4">
              <DialogHeader className="p-4 pt-2 pb-2">
                 <DialogTitle className="text-lg font-headline">Original Receipt</DialogTitle>
-                 <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-                    <X className="h-4 w-4" />
-                    <span className="sr-only">Close</span>
-                </DialogClose>
             </DialogHeader>
             <div className="max-h-[80vh] overflow-y-auto px-4 pb-4">
                 <img 
