15a08e8efba4b50b3317913b5439c9fe719bd685
Oui Wutcharin <wutcharin.th@gmail.com>
2025-09-30 07:15:45 +0000

    It's still loading forever.

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -5,7 +5,7 @@ import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
     "projectId": "billzai",
     "appId": "1:743957871307:web:a6ed450e23c737a43ff94e",
-    "storageBucket": "billzai.firebasestorage.app",
+    "storageBucket": "billzai.appspot.com",
     "apiKey": "AIzaSyDPFEoxyFrcJwWeQk6aVjK8YbPodmxLA2o",
     "authDomain": "billzai.firebaseapp.com",
     "measurementId": "G-YTWRN4GFLT",
